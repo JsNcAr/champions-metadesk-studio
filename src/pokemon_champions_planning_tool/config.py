3cd33e9bc6c9e97c2318fbb22cd929f9855a1d25
JsNcAr <jn.arias@uniandes.edu.co>
2026-07-12 03:17:43 -0500

    feat: add configuration file with project-wide settings and defaults

---
+++ src/pokemon_champions_planning_tool/config.py
@@ -0,0 +1,22 @@
+"""Project-wide settings and storage defaults."""
+
+APP_NAME = "Pokemon Champions Planning Tool"
+DEFAULT_CSV_FILENAME = "pokemon_team_stats.csv"
+POKEAPI_BASE_URL = "https://pokeapi.co/api/v2"
+POKEAPI_TIMEOUT_SECONDS = 5
+
+CSV_HEADERS = [
+    "Pokémon",
+    "Form",
+    "Ability",
+    "HP",
+    "Attack",
+    "Defense",
+    "Sp. Atk",
+    "Sp. Def",
+    "Speed",
+    "Total",
+]
+
+# Backward-compatible alias while the codebase is still mid-refactor.
+HEADERS = CSV_HEADERS
